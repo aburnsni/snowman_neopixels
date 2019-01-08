8ffc84fd0caf7287a8e09ea403a942a398d89580
Andrew Burns <aburnsni@gmail.com>
2019-01-08 16:13:08 +0000

    Added uno and colour switching

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,20 @@
 #include <NeoPixelBus.h>
 
 //Switch setup
-const uint8_t switchPin = 0;
+const uint8_t switchPin = 5;
 
 //NeoPixel Setup
 const uint16_t PixelCount = 5;
 const uint8_t PixelPin = 2;
-#define brightness 0.7 // Between 0 and 1
+#define brightness 0.2 // Between 0 and 1
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
 
 // Set rgb colours
 RgbColor white(255 * brightness);
-RgbColor red(255 * brightness,0,0);
-RgbColor green(0,255 * brightness,0);
-RgbColor blue(0,0,255 * brightness);
+RgbColor red(255 * brightness, 0, 0);
+RgbColor green(0, 255 * brightness, 0);
+RgbColor blue(0, 0, 255 * brightness);
+RgbColor purple(255*brightness/2, 0, 255*brightness/2);
 RgbColor black(0);
 
 void setup() {
@@ -32,7 +33,13 @@ void loop() {
     strip.Show();
     delay(100);
   } else {
-    //switch low colour change
+    strip.SetPixelColor(0, black);
+    strip.SetPixelColor(1, red);
+    strip.SetPixelColor(2, green);
+    strip.SetPixelColor(3, blue);
+    strip.SetPixelColor(4, purple);
 
+    strip.Show();
+    delay(100);
   }
 }
